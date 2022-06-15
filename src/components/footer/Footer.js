50b0eb0637f37a5070f65a5e60952a2e52f5362a
Aayushojha <18egjcs024@gitjaipur.com>
2022-06-15 09:03:29 +0530

    footer updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-light text-center text-lg-start">
+    <footer className="bg-dark text-center text-lg-start">
       <div
-        className="text-center p-3"
+        className="text-center p-3 text-light"
         style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
       >
         © 2020 Copyright:
-        <a className="text-dark" href="https://mdbootstrap.com/">
+        <a className="text-light m-2" href="https://mdbootstrap.com/">
           MDBootstrap.com
         </a>
       </div>
